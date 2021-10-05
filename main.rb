a14bbe5f21434fe3efd78e4bc08b810da57b7fcd
Mathew McKinney <mckiney.m93@gmail.com>
2021-10-04 20:59:56 -0700

    game over triggers on countdown, and next level triggers when player touches exit!

--- main.rb
+++ main.rb
@@ -9,14 +9,10 @@ class Game
     @args = args
     @character = Character.new(args)
     @board = GameBoard.new(args)
-    @player_rect = {
-                    x: @character.player_x_position, y: @character.player_y_position, 
-                    w: @character.player_w, h: @character.player_h
-                  }
     @exit_rect = {
-                  x: @board.exit_x_position, y: @board.exit_y_position, 
-                  w: @board.exit_w, h: @board.exit_h
-                }
+      x: @board.exit_x_position, y: @board.exit_y_position,
+      w: @board.exit_w, h: @board.exit_h
+    }
   end
 
   def render
@@ -25,13 +21,17 @@ class Game
   end
 
   def game_state
-    @args.outputs.labels << [640, 400, 'Next Level', 100, 1, 0, 255, 0] if @player_rect.intersect_rect? @exit_rect
+    @args.outputs.labels << [640, 400, 'Next Level', 100, 1, 0, 255, 0] if @player_rect.intersect_rect?(@exit_rect)
     @args.outputs.labels << [640, 400, 'GAME OVER', 100, 1, 0, 255, 0] if @board.time <= 0
   end
 
   def tick
     render
     @character.move
+    @player_rect = {
+      x: @character.player_x_position, y: @character.player_y_position,
+      w: @character.player_w, h: @character.player_h
+    }
     game_state
   end
 end
